9027c8a3402bc7a9b15bb21769e6819949b01f50
Lucas Santos <jl.lucassantos098@gmail.com>
2023-08-29 17:39:39 -0300

    final :)

--- digisac/management/commands/get_protocols.py
+++ digisac/management/commands/get_protocols.py
@@ -4,19 +4,26 @@ from digisac.api_digisac import request_utils
 from protocols.update_database import update
 from protocols.models import Protocol
 
+
 def get_protocol_times(protocol_number):
     try:
         # Pesquisar o protocolo com base no número fornecido
         protocol = Protocol.objects.get(protocol_number=protocol_number)
-        return f'primeiro tempo de espera : {protocol.first_waiting_time}\n', f'tempo total :{protocol.total_attendance_time}\n', f'média: {protocol.average_waiting_time}\n'
+        return (
+            f"primeiro tempo de espera : {protocol.first_waiting_time}\n",
+            f"tempo total :{protocol.total_attendance_time}\n",
+            f"média: {protocol.average_waiting_time}\n",
+        )
 
     except Protocol.DoesNotExist:
         return None, None, None
 
+
 class Command(BaseCommand):
     def handle(self, *args: Any, **options: Any) -> str | None:
-        api = request_utils.DigisacConfigRequest()
-        json = api.get_historic(start_period='03/01/2023', end_period='04/01/2023')
-        # json = api.get_historic()
-        update.update(json)
-        # print(get_protocol_times('2023080335351'))
+        print("sda")
+        # api = request_utils.DigisacConfigRequest()
+        # json = api.get_historic(start_period='03/01/2023', end_period='04/01/2023')
+        # # json = api.get_historic()
+        # update.update(json)
+        # # print(get_protocol_times('2023080335351'))
